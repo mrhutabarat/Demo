f8591bd3e32d3c3b55edfb46855d67b7f5981daf
leonardushutabarat <leonardus.hutabarat@gmail.com>
2021-06-27 11:27:02 +0100

    Demo - initial commit

---
+++ Demo/src/co/uk/leonardushutabarat/Demo.java
@@ -0,0 +1,19 @@
+package co.uk.leonardushutabarat;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Temp\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.uk/");
+		driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click();
+		driver.getTitle();
+
+	}
+
+}
